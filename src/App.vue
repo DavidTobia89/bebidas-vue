3b81bfa9a4ad64b585ae086e26cd06640d030a7a
DavidTobia89 <dtobia@tragsa.es>
2024-02-06 09:56:22 +0100

    se empieza a crear el formulario

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
-
+import { RouterView } from 'vue-router';
+import Header from './components/Header.vue'
 
 </script>
 
 <template>
- 
+ <Header></Header>
+ <main class="container mx-auto mt-10">
+  <RouterView></RouterView>
+ </main>
 </template>
 
 <style scoped>
